c8f60496277278a154b3aa1770456349f2c4c671
jonGFprog <jongf2005@gmail.com>
2025-10-28 18:14:43 +0100

    pruebas frecuencia del reloj por parametro

--- Hilos/clock.c
+++ Hilos/clock.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-
+#include <unistd.h>
 void* clock_thread(void* arg) {
+    __uint128_t i=0;
     printf("Reloj iniciado\n");
     while(1){
-        
+        int *hz_clock=arg;
+        usleep(hz_clock[0]); //usa microsegundos, 1 seg=1000000
+        i++;
+        printf("ciclo %d\n",i);
     }
 }
 
--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <pthread.h>
+#include <stdlib.h>
 #include "Hilos/clock.c"
 int main (int argc, char *argv[]) {
+    if(argc!=2){
+        printf("Numero incorrecto de argumentos.\n");
+        exit(1);
+    }
     pthread_t clock_id;
-    pthread_create(&clock_id,NULL,clock_thread, NULL);
+    int hz_clock=atoi(argv[1]); //esta en milisegundos
+    pthread_create(&clock_id,NULL,clock_thread, &hz_clock);
     pthread_join(clock_id,NULL);
 }
