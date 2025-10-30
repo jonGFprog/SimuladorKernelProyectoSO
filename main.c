a9194ea07a8f49a1b7008460edf6518065ae9dbe
jonGFprog <jongf2005@gmail.com>
2025-10-30 16:24:43 +0100

    pruebas con 2 timers

--- main.c
+++ main.c
@@ -5,12 +5,13 @@
 #include <string.h>
 
 int main (int argc, char *argv[]) {
-    if(argc!=4){
+    if(argc!=6){
         printf("Numero incorrecto de argumentos.\n");
         exit(1);
     }
     pthread_t clock_id;
     pthread_t timer_id;
+    pthread_t timer2_id;
     t_clock_args clock_args;
     clock_args.frec=atoi(argv[1]); //esta en milisegundos
 
@@ -18,10 +19,17 @@ int main (int argc, char *argv[]) {
     timer_args.ciclos=atoi(argv[3]);
     timer_args.msg=malloc(strlen(argv[2]));
     strcpy(timer_args.msg,argv[2]);
-    pthread_create(&clock_id,NULL,clock_thread, &clock_args);
- 
-    pthread_create(&timer_id,NULL,timer_thread, &timer_args);
     
-    pthread_join(timer_id,NULL);    
+    t_timer_args timer2_args;
+    timer2_args.ciclos=atoi(argv[5]);
+    timer2_args.msg=malloc(strlen(argv[4]));
+    strcpy(timer2_args.msg,argv[4]);
+    
+    pthread_create(&clock_id,NULL,clock_thread, &clock_args);
+    pthread_create(&timer_id,NULL,timer_thread, &timer_args);
+    pthread_create(&timer2_id,NULL,timer_thread, &timer2_args);    
+
+    pthread_join(timer_id,NULL);
+    pthread_join(timer2_id,NULL);    
     pthread_join(clock_id,NULL);
 }
