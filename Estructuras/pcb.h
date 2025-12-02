d1ec0c93871faa5d54736b837ad66d4abe1f86d0
jonGFprog <jongf2005@gmail.com>
2025-12-02 17:04:05 +0100

    detallitos del scheduler

--- Estructuras/pcb.h
+++ Estructuras/pcb.h
@@ -1,6 +1,10 @@
 typedef struct
 {
     __uint64_t id;
-    __uint64_t life;
+    __uint8_t partido; //0 si no es parte del partido, 1 si lo es
+    __uint16_t ciclos_asignados; //ciclos maximos antes de su ejecucion publica
+    __uint16_t ciclos_usados;  
+    __uint16_t quantum; //tiempo de cpu asignado por el lider
+    __uint16_t paciencia; //solo importante si es un proceso del partido
 } t_pcb;
 
